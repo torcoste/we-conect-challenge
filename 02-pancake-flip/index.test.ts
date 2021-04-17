50a9d39f5088657e03b564e5b8479922a4f3c83b
Victor Kostyuk <torcoste@gmail.com>
2021-04-17 23:03:02 +0300

    [task-02] sample test case input typofix

--- 02-pancake-flip/index.test.ts
+++ 02-pancake-flip/index.test.ts
@@ -1,7 +1,7 @@
 import process from "./index"
 
 it("pass sample test case", () => {
-  const inputString = "3\n—+-++- 3\n+++++ 4\n-+-+- 4"
+  const inputString = "3\n—--+-++- 3\n+++++ 4\n-+-+- 4"
   const expectedOutput = "Case #1: 3\nCase #2: 0\nCase #3: -1"
   expect(process(inputString)).toBe(expectedOutput)
 })
